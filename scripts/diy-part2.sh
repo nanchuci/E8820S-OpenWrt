c8c7a897838e424c4f463853e7d684469f7f4395
nanchuci <71806013+nanchuci@users.noreply.github.com>
2021-03-05 00:39:44 +0800

    e8820s云编译LEDE

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -130,17 +130,17 @@ svn co https://github.com/immortalwrt/packages/trunk/admin/netdata
 popd
 
 # Mod zzz-default-settings
-pushd package/lean/default-settings/files
-sed -i '/http/d' zzz-default-settings
-export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
-sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
-popd
+#pushd package/lean/default-settings/files
+#sed -i '/http/d' zzz-default-settings
+#export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+#sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+#popd
 
 # Fix libssh
-pushd feeds/packages/libs
-rm -rf libssh
-svn co https://github.com/openwrt/packages/trunk/libs/libssh
-popd
+#pushd feeds/packages/libs
+#rm -rf libssh
+#svn co https://github.com/openwrt/packages/trunk/libs/libssh
+#popd
 
 # Use Lienol's https-dns-proxy package
 pushd feeds/packages/net
@@ -166,4 +166,4 @@ make && sudo make install
 popd
 
 # Change default shell to zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+#sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
